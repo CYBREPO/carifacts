d26620fda624714a8f1008cd1e58e9d20da4c906
RoronoaZoro34 <shivanaik2398@gmail.com>
2023-05-25 22:06:29 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModuleModule } from './shared/shared-module.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 import { CardComponent } from './components/card/card.component';
+import { LayoutModuleModule } from './layout/layout-module.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    CardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModuleModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/components-module.module.ts
+++ src/app/components/components-module.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapComponent } from './map/map.component';
+import { CardComponent } from './card/card.component';
 
 
 
 @NgModule({
   declarations: [
-    MapComponent
+    MapComponent,
+    CardComponent,
   ],
   imports: [
     CommonModule
