bd7272200d367ad19f58b455f372e199916a6a5f
chaitanyashirodkar010 <chaitanyashirodkar010@gmail.com>
2023-07-10 19:36:48 +0530

    iamges

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -80,6 +80,7 @@ export class HeaderComponent implements OnInit {
     this.router.navigate(['account/login']);
   }
   goback() {
+    this.router.navigate(['/']);
     window.location.reload();
   }
 }
